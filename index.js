e51936136fa34960d25eb08159c35c121f5d7384
GorenganHunter <gorengandev@gmail.com>
2023-06-04 14:31:42 +0700

    Refresh leaderboard data every 1 minute

--- index.js
+++ index.js
@@ -1,38 +1,53 @@
-const path = require('path');
-const express = require('express')
-const bodyParser = require('body-parser')
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
-const leaderboard = []
+let leaderboard = [];
 // Fetch HTML content from the URL
-JSDOM.fromURL("https://lurkr.gg/levels/1054414599945998416")
-  .then(dom => {
-    const names = dom.window.document.querySelectorAll("td:nth-child(2) span")
-    const levels = dom.window.document.querySelectorAll("td:last-child span")
-    names.forEach((name, i) => leaderboard.push({ name: name.textContent, level: levels[i].textContent }))
-    console.log(leaderboard)
-  })
-  .catch(error => {
-    console.error("Error fetching HTML content:", error);
-  });
+const refreshData = () => {
+  leaderboard = [];
+  JSDOM.fromURL("https://lurkr.gg/levels/1054414599945998416")
+    .then((dom) => {
+      const names = dom.window.document.querySelectorAll(
+        "td:nth-child(2) span"
+      );
+      const levels = dom.window.document.querySelectorAll("td:last-child span");
+      names.forEach((name, i) =>
+        leaderboard.push({
+          name: name.textContent,
+          level: levels[i].textContent,
+        })
+      );
+      console.log(leaderboard);
+    })
+    .catch((error) => {
+      console.error("Error fetching HTML content:", error);
+    });
+};
 
-const app = express()
+refreshData();
 
-app.set('view engine', 'ejs')
-app.use(express.static(path.join(__dirname, '/public')));
+setInterval(refreshData, 60000);
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended: true
-}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-	res.render("home")
-})
+  res.render("home");
+});
 
-app.listen(8080, (req, res) => {
-    Host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-    console.log("Server is running on port 8080")
-})
+app.listen(
+  8080,
+  process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
+  (req, res) => {
+    console.log("Server is running on port 8080");
+  }
+);
