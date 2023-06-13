40b4a8a748a08ecadd80241c8c8f06c55bcb5c34
GorenganHunter <gorengandev@gmail.com>
2023-06-14 05:59:19 +0700

    Mengirim saran ke channel discord

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+
 require("dotenv").config();
 
 const { JSDOM } = require("jsdom");
+
+const { Client } = require("discord.js");
+const client = new Client({ intents: 131071 });
+
+client.config = require("./config.json");
+
+const getChannelSaran = async () => {
+  const channelSaran = {
+    saranDiscord: await client.channels.fetch(
+      client.config.discordChannelId.saranDiscord
+    ),
+    saranYoutube: await client.channels.fetch(
+      client.config.discordChannelId.saranYoutube
+    ),
+    saranWebsite: await client.channels.fetch(
+      client.config.discordChannelId.saranWebsite
+    ),
+  };
+  return channelSaran;
+};
 
 let leaderboard = [];
 
@@ -12,7 +32,9 @@ const refreshData = () => {
   JSDOM.fromURL("https://lurkr.gg/levels/1054414599945998416")
     .then((dom) => {
       leaderboard = [];
-      const names = dom.window.document.querySelectorAll("td:nth-child(2) span");
+      const names = dom.window.document.querySelectorAll(
+        "td:nth-child(2) span"
+      );
       const levels = dom.window.document.querySelectorAll("td:last-child span");
       const imgs = dom.window.document.querySelectorAll(".gap-4 > img");
 
@@ -36,11 +58,9 @@ setInterval(refreshData, 60000);
 
 const app = express();
 
-const saranRouter = require("./routes/saran")
-
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "/public")));
-app.use(express.static(path.join(__dirname, "/node_modules/bootstrap/dist")))
+app.use(express.static(path.join(__dirname, "/node_modules/bootstrap/dist")));
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -58,25 +78,29 @@ app.get("/leaderboard", function (req, res) {
   });
 });
 
-const uri = process.env.MONGODBURI;
 const port = 8080;
 
-mongoose
-  .connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 5000, // Increase the server selection timeout
-  })
-  .then(() => {
-    console.log("Connected to the database");
-    app.use("/saran", saranRouter)
+client
+  .login(process.env.DISCORDBOTTOKEN)
+  .then(async () => {
+    console.log(`Logged in as ${client.user.tag}`);
+
+    const channelSaran = await getChannelSaran();
+
+    const SaranRouter = require("./routes/Saran.js");
+
+    const saranRouter = new SaranRouter(channelSaran).getRouter();
+
+    app.use("/saran", saranRouter);
+
     app.listen(port, () => {
       console.log(`App is running on port ${port}`);
     });
   })
-  .catch((error) => {
-    console.error("Database connection error:", error);
-    console.log("Disabled /saran");
+  .catch((err) => {
+    console.error("Cannot login to discord", err);
+    console.log("Disable /saran");
+
     app.listen(port, () => {
       console.log(`App is running on port ${port}`);
     });
