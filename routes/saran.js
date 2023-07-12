a593ca901c3b4d62a6a8d7e6b36d59fc1091b5ab
GorenganHunter <gorengandev@gmail.com>
2023-07-12 14:22:47 +0700

    Hanya bisa mengirim 1 saran per kategori setiap jam

--- routes/saran.js
+++ routes/saran.js
@@ -8,10 +8,15 @@ const webhookSaran = {
   saranDiscord: new WebhookClient({ url: process.env.WEBHOOK_SARAN_DISCORD }),
   saranYoutube: new WebhookClient({ url: process.env.WEBHOOK_SARAN_YOUTUBE }),
   saranWebsite: new WebhookClient({ url: process.env.WEBHOOK_SARAN_WEBSITE }),
-}
+};
 
 function homeCtrl(req, res) {
-  res.render("saran", { title: "Saran", webhookSaran, success: req.success || false, err: req.err || "" });
+  res.render("saran", {
+    title: "Saran",
+    webhookSaran,
+    success: req.success || false,
+    err: req.err || "",
+  });
 }
 
 function postCtrl(req, res, next) {
@@ -20,8 +25,8 @@ function postCtrl(req, res, next) {
   const saran = req.body.saran;
 
   if (!jenis) {
-    req.err = "jenis kosong"
-    return next()
+    req.err = "jenis kosong";
+    return next();
   }
 
   const webhook =
@@ -39,8 +44,8 @@ function postCtrl(req, res, next) {
   }
 
   if (!saran) {
-    req.err = "saran kosong"
-    return next()
+    req.err = "saran kosong";
+    return next();
   }
 
   webhook
@@ -56,19 +61,64 @@ function postCtrl(req, res, next) {
     });
 }
 
-const sendSuggestLimiter = rateLimit({
-	windowMs: 60 * 60 * 1000, // 1 hour
-	max: 1, // Limit each IP to 1 suggest per `window` (here, per hour)
+const sendDiscordSuggestLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 1 hour
+  max: 1, // Limit each IP to 1 suggest per `window` (here, per hour)
   handler(req, res) {
-    req.err = "limit"
-    return homeCtrl(req, res)
+    req.err = "limit";
+    return homeCtrl(req, res);
   },
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-})
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+const sendYoutubeSuggestLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 1 hour
+  max: 1, // Limit each IP to 1 suggest per `window` (here, per hour)
+  handler(req, res) {
+    req.err = "limit";
+    return homeCtrl(req, res);
+  },
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+const sendWebsiteSuggestLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 1 hour
+  max: 1, // Limit each IP to 1 suggest per `window` (here, per hour)
+  handler(req, res) {
+    req.err = "limit";
+    return homeCtrl(req, res);
+  },
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
 app.get("/", homeCtrl);
 
-app.post("/", sendSuggestLimiter, postCtrl, homeCtrl);
+app.post("/", (req, res, next) => {
+  const jenis = req.body.jenis;
+
+  if (!jenis) {
+    req.err = "jenis kosong";
+    return homeCtrl(req, res);
+  }
+
+  const limiter =
+    jenis == "discord"
+      ? sendDiscordSuggestLimiter
+      : jenis == "youtube"
+      ? sendYoutubeSuggestLimiter
+      : jenis == "website"
+      ? sendWebsiteSuggestLimiter
+      : undefined;
+
+  if (!limiter) {
+    req.err = `no webhook ${jenis}`;
+    return homeCtrl(req, res);
+  }
+
+  return limiter(req, res, next)
+}, postCtrl, homeCtrl);
 
 module.exports = app;
